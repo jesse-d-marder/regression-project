60a1cc9e10bea34d45697eecf7eed15923fd5402
Jesse Marder <jesse.d.marder@gmail.com>
2022-03-28 15:56:23 -0500

    Initial commit

---
+++ model.py
@@ -0,0 +1,191 @@
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LassoLars, LinearRegression, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+
+import pandas as pd
+import wrangle
+
+
+def model_feature_selection(train, validate, test, to_dummy, features_to_scale, columns_to_use):
+    
+    X_train_exp = pd.get_dummies(train, columns = to_dummy, drop_first=True)
+    
+    X_train = X_train_exp[columns_to_use]
+    X_validate = pd.get_dummies(validate, columns = to_dummy, drop_first=True)[columns_to_use]
+    X_test = pd.get_dummies(test, columns = to_dummy, drop_first=True)[columns_to_use]
+    
+    X_train_scaled, X_validate_scaled, X_test_scaled = wrangle.scale_data(X_train, X_validate, X_test,features_to_scale)
+    
+    y_train = train[['tax_value']]
+    y_validate = validate[['tax_value']]
+    y_test = test[['tax_value']]
+    
+    # Perform Feature Selection using Recursive Feature Elimination
+    # Initialize ML algorithm
+    lm = LinearRegression()
+    # create RFE object - selects top 3 features only
+    rfe = RFE(lm, n_features_to_select=5)
+    # fit the data using RFE
+    rfe.fit(X_train_scaled, y_train)
+    # get mask of columns selected
+    feature_mask = rfe.support_
+    # get list of column names
+    rfe_features = X_train_scaled.iloc[:,feature_mask].columns.tolist()
+    # view list of columns and their ranking
+
+    # get the ranks
+    var_ranks = rfe.ranking_
+    # get the variable names
+    var_names = X_train_scaled.columns.tolist()
+    # combine ranks and names into a df for clean viewing
+    rfe_ranks_df = pd.DataFrame({'Var': var_names, 'Rank': var_ranks})
+    # sort the df by rank
+    rfe_ranks_df.sort_values('Rank')
+
+    return X_train_scaled, X_validate_scaled, X_test_scaled, y_train, y_validate, y_test, rfe_features
+    
+def model(X_train_scaled, X_validate_scaled, X_test_scaled, y_train, y_validate, y_test,rfe_features):
+    
+    ### BASELINE
+    
+    # 1. Predict tax_value_pred_mean
+    tax_value_pred_mean = y_train['tax_value'].mean()
+    y_train['tax_value_pred_mean'] = tax_value_pred_mean
+    y_validate['tax_value_pred_mean'] = tax_value_pred_mean
+
+    # 2. compute tax_value_pred_median
+    tax_value_pred_median = y_train['tax_value'].median()
+    y_train['tax_value_pred_median'] = tax_value_pred_median
+    y_validate['tax_value_pred_median'] = tax_value_pred_median
+
+    # 3. RMSE of tax_value_pred_mean
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_mean)**(1/2)
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_mean)**(1/2)
+
+    print("RMSE using Mean\nTrain/In-Sample: ", round(rmse_train, 2), 
+          "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+
+    # 4. RMSE of tax_value_pred_median
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_median)**(1/2)
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_median)**(1/2)
+
+    print("RMSE using Median\nTrain/In-Sample: ", round(rmse_train, 2), 
+          "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+
+    ### OLS Linear Regression
+    
+    # create the model object
+    lm = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm.fit(X_train_scaled, y_train.tax_value)
+
+    # predict train
+    y_train['tax_value_pred_lm'] = lm.predict(X_train_scaled)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lm)**(1/2)
+
+    # predict validate
+    y_validate['tax_value_pred_lm'] = lm.predict(X_validate_scaled)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lm)**(1/2)
+
+    print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+
+
+    # Lasso-Lars
+    
+    # create the model object
+    lars = LassoLars(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lars.fit(X_train_scaled, y_train.tax_value)
+
+    # predict train
+    y_train['tax_value_pred_lars'] = lars.predict(X_train_scaled)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lars)**(1/2)
+
+    # predict validate
+    y_validate['tax_value_pred_lars'] = lars.predict(X_validate_scaled)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lars)**(1/2)
+
+    print("RMSE for OLS using LarsLasso\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+
+
+    # Tweedie
+    
+    # create the model object
+    glm = TweedieRegressor(power=1, alpha=0)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    glm.fit(X_train_scaled, y_train.tax_value)
+
+    # predict train
+    y_train['tax_value_pred_glm'] = glm.predict(X_train_scaled)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_glm)**(1/2)
+
+    # predict validate
+    y_validate['tax_value_pred_glm'] = glm.predict(X_validate_scaled)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_glm)**(1/2)
+
+    print("RMSE for GLM using Tweedie, power=1 & alpha=0\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+
+    # Polynomial features
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=2,interaction_only=True)
+
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(X_train_scaled)
+
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(X_validate_scaled)
+    X_test_degree2 = pf.transform(X_test_scaled)
+    
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, y_train.tax_value)
+
+    # predict train
+    y_train['tax_value_pred_lm2'] = lm2.predict(X_train_degree2)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.tax_value, y_train.tax_value_pred_lm2)**(1/2)
+
+    # predict validate
+    y_validate['tax_value_pred_lm2'] = lm2.predict(X_validate_degree2)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.tax_value, y_validate.tax_value_pred_lm2)**(1/2)
+
+    print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+
+    results = pd.concat([
+    y_validate.apply(lambda col: r2_score(y_validate.tax_value, col)).rename('r2'),
+    y_validate.apply(lambda col: mean_squared_error(y_validate.tax_value, col)).rename('mse'),
+    ], axis=1).assign(
+        rmse=lambda df: df.mse.apply(lambda x: x**0.5)
+    )
+    
+    return results
+
